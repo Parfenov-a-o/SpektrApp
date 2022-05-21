b1137521d9bc64780f729f9f8417f433e0f8a9d9
Aleksandr <parfenov.a.o@mail.ru>
2022-05-21 12:00:55 +0700

    Начал разработку ClientViewModel

---
+++ SpektrApp/ViewModels/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace SpektrApp.ViewModels
+{
+    internal class BaseViewModel:INotifyPropertyChanged
+    {
+        //Реализация интерфейса INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
