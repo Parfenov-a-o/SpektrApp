42e56ea9bfe645ce45bd648de5b6d2358ed3d243
Alexey-Androsov <alexey_androsov_322@mail.ru>
2022-05-28 12:33:46 +0300

    Создали папки для новых окон

---
+++ SpektrApp/Views/AddService/AddMaintainedCompletedProject/AdditionalMaintained/ChooseEmployeeView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SpektrApp.Views.AddService.AddMaintainedCompletedProject.AdditionalMaintained
+{
+    /// <summary>
+    /// Логика взаимодействия для ChooseEmployeeView.xaml
+    /// </summary>
+    public partial class ChooseEmployeeView : Window
+    {
+        public ChooseEmployeeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SpektrApp/Views/InfoUser/UserRoleView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SpektrApp.Views.InfoUser
+{
+    /// <summary>
+    /// Логика взаимодействия для UserRoleView.xaml
+    /// </summary>
+    public partial class UserRoleView : Window
+    {
+        public UserRoleView()
+        {
+            InitializeComponent();
+        }
+    }
+}
